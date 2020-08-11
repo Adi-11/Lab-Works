d78bc17a65c7616963c415111bc18a4f71e10121
Aditya <adityaagupta31@gmail.com>
2020-08-11 18:37:49 +0530

    minor changes

--- DS-cpp/knapSackDP.cpp
+++ DS-cpp/knapSackDP.cpp
@@ -23,6 +23,7 @@ void solve()
         cin >> wt[i];
 
     memset(dp, 0, sizeof(dp));
+
     for (int i = 1; i < n + 1; i++) {
         for (int j = 1; j < max_wt + 1; j++) {
             if (wt[i - 1] <= max_wt)
