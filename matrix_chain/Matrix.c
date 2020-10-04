40185f91ada38035a1f2eaa820ceead87b6e360e
Aditya <adityaagupta31@gmail.com>
2020-10-04 16:21:57 +0530

    updated MCM

--- matrix_chain/Matrix.c
+++ matrix_chain/Matrix.c
@@ -1,47 +1,60 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define INF 999999
 
-int MatrixChainMult(int arr[], int n)
-{
-
-	int A[n][n];
-
-	int i, j, k, L, q;
-
-	for (i=1; i<n; i++)
-		A[i][i] = 0;
-
-	for (L=2; L<n; L++)
-	{
-		for (i=1; i<n-L+1; i++)
-		{
-			j = i+L-1;
-			A[i][j] = 999999;
-			for (k=i; k<=j-1; k++)
-			{
-				q = A[i][k] + A[k+1][j] + arr[i-1]*arr[k]*arr[j];
-				if (q < A[i][j])
-					A[i][j] = q;
-			}
-		}
-	}
-
-	return A[1][n-1];
+int min(int a, int b){
+    return (a < b) ? a : b;
 }
 
-int main()
-{
-	int n,i, result;
-    printf("Enetr the Size of the array:\n");
-    scanf("%d",&n);
-    int arr[n];
-    printf("Enter the elements of the array:\n");
-    for ( i = 0; i < n; i++)
-        scanf("%d",&arr[i]);
 
-    result = MatrixChainMult(arr, n);
+int matrixChain(int *arr, int n){
+    int dp[n + 1][n + 1]; //for Tabulation
 
-    printf("The minimum number of scalar multiplication needed for chain of matrix\n");
-    printf("%d",result);
-	return 0;
+    int i;
+    for(i = 1; i < n; i++){
+        dp[i][i] = 0;
+    }
+
+    int len, j, k;
+    len = 2;
+    while(len < n){
+        for(i = 1; i <= (n - len); i++){
+            int j = (len + i - 1);
+            dp[i][j] = INF;
+            for(k = i; k < j; k++){
+                dp[i][j] = min(dp[i][j], (dp[i][k] + dp[k + 1][j] + arr[i - 1] * arr[k] * arr[j]));
+            }
+        }
+        len += 1;
+    }
+    return dp[1][n - 1];
+
 }
+
+int main(int argc, char const *argv[])
+{
+    
+
+    int n;
+    printf("Enter the array length: \n");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(sizeof(int) * n);
+    int i;
+    printf("Enter the array elements: \n");
+    for(i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    clock_t tic = clock();
+    int result = matrixChain(arr, n);
+    printf("Minimum number of multiplications is: %d\n", result);
+    clock_t toc = clock();
+
+
+    printf("Time of execution: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+    return 0;
+}
+
+// 5 10 3 12 5 50 6
